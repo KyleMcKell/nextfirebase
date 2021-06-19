99bf5fe796ff468bc063ec84c0a83daa76564be2
Kyle McKell <mckellkylea@gmail.com>
2021-06-19 17:09:10 -0600

    added navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,1 +1,46 @@
-export {};
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+interface Props {}
+
+const Navbar = (props: Props) => {
+	const { user, username }: any = {};
+
+	return (
+		<nav className="navbar">
+			<ul>
+				<li>
+					<Link href="/" passHref>
+						<button>FEED</button>
+					</Link>
+				</li>
+				{/* user is signed in and has username */}
+				{username && (
+					<>
+						<li>
+							<Link href="/admin" passHref>
+								<button className="btn-blue">Write Posts</button>
+							</Link>
+						</li>
+						<li>
+							<Link href={`/${username}`} passHref>
+								<Image src={user?.photoURL} alt={username} />
+							</Link>
+						</li>
+					</>
+				)}
+				{/* user is not signed OR has not created username */}
+				{!username && (
+					<li>
+						<Link href="/enter" passHref>
+							<button className="btn-blue">Log in</button>
+						</Link>
+					</li>
+				)}
+			</ul>
+		</nav>
+	);
+};
+
+export default Navbar;
